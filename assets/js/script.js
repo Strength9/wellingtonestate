7c96d3a49237465e832edc24864a909640f92b35
Strength9 <Strength9@users.noreply.github.com>
2023-07-28 11:18:09 +0100

    Community SLider added

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,3 +112,17 @@ if ($(".jobssslider").length > 0) {
       jobsplide.mount();
   } );
  }
+
+
+if ($(".comslider").length > 0) {
+   document.addEventListener( 'DOMContentLoaded', function() {  
+       var comslider = new Splide( '.comslider', {
+       type  : 'fade',
+       rewind: true,
+       arrowPath: 'M422.6 278.6L445.3 256l-22.6-22.6-144-144L256 66.7 210.8 112l22.6 22.6L322.8 224 32 224 0 224l0 64 32 0 290.7 0-89.4 89.4L210.8 400 256 445.3l22.6-22.6 144-144z',
+      autoplay:true,
+ 
+     } );
+      comslider.mount();
+   } );
+  }
---
+++ template-parts/block/community_slider.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Block Name: Community Slider
+Block Description: Community Slider
+Post Types: post, page, custom-type
+Block SVG: block_template.svg
+Block Category: wellington
+    
+    
+
+*/
+
+$titleblock = s9ACF_textfield('title_area', '', '', '', '', 'no');
+$slider_sub_title = s9ACF_textfield('slider_sub_title', '', '', '', '', 'no');
+
+ // Check rows existexists.
+ if( have_rows('community_slider') ) {
+ 
+ $slides = ' <div class="splide__track"><ul class="splide__list">';
+     // Loop through rows.
+     while( have_rows('community_slider') ) : the_row();
+ 
+
+        if( get_sub_field('side_image') ) {
+             $imgbg = 'style="background-image:url('.get_sub_field('side_image').');"';
+         } else {
+            $imgbg = '';
+         }
+         
+         
+         $linkbut = '';
+          $link = get_sub_field('slider_link');
+          if( $link ): 
+              $link_url = $link['url'];
+              $link_title = $link['title'];
+              $link_target = $link['target'] ? $link['target'] : '_self';
+             
+             $linkbut = '<a href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.esc_html( $link_title ).'</a>';
+          endif;
+          
+     
+ $slidertitle = get_sub_field('slider_title');
+  $slidertext = get_sub_field('slider_text');;
+ $sliderlink = get_sub_field(' slider_link');
+         
+         // Do something...
+         $slides .= '<li class="splide__slide">  
+             <div class="imageside" '.$imgbg .'> </div>
+             <div class="slidedatabox">
+                    <hr>
+                    <div class="info">
+                    <img src="/wp-content/uploads/2023/07/Duke.png">
+                    <h4>'.$slider_sub_title.'</h4>
+                    <h2>'.$slidertitle.'</h2>
+                    <p>'.$slidertext.'</p>
+                    '.$linkbut.'
+                    </div>
+               </div>
+         </li>';
+     // End loop.
+     endwhile;
+ $slides .= '</ul> </div>';
+ // No value.
+ } else {
+    $slides = 'Add Sliider Details';
+ };
+ 
+ 
+  ?>
+ <section class="comssliderintro" >
+        <h1><?php echo get_field('title_area');?></h1>
+ </section>
+ <section class="splide comslider" >
+   
+    
+        <?php echo $slides;?>
+   
+  </section>
